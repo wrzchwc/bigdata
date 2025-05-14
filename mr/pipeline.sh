bf449f1af33b7fc64218b88c6e266c7a0e97c9be
Jakub Wierzchowiec <wierzchowieckuba@gmail.com>
2025-05-14 02:15:15 +0200

    time logging

--- mr/pipeline.sh
+++ mr/pipeline.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOGFILE="job_timing.log"
+
 HADOOP_STREAMING_JAR=/hadoop/hadoop-dist/target/hadoop-3.5.0-SNAPSHOT/share/hadoop/tools/lib/hadoop-streaming-3.5.0-SNAPSHOT.jar
 
 INPUT_MIMIC=/project/mimic/mimic_data.csv
@@ -12,35 +14,50 @@ FINAL_OUT=/output/final
 
 hdfs dfs -rm -r -f $TMP1_OUT $TMP2_OUT $TMP3_OUT $FINAL_OUT
 
-hadoop jar $HADOOP_STREAMING_JAR \
-  -input $INPUT_MIMIC \
-  -output $TMP1_OUT \
-  -mapper mapper_mimic.py \
-  -reducer cat \
-  -file mapper_mimic.py
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] START $1" >> $LOGFILE
+  START_TIME=$(date +%s)
 
-hadoop jar $HADOOP_STREAMING_JAR \
-  -input $INPUT_GDELT \
-  -output $TMP2_OUT \
-  -mapper mapper_gdelt.py \
-  -reducer reducer_gdelt.py \
-  -file mapper_gdelt.py \
-  -file reducer_gdelt.py
+  eval "$2"
 
-hadoop jar $HADOOP_STREAMING_JAR \
-  -input $TMP1_OUT \
-  -input $TMP2_OUT \
-  -output $TMP3_OUT \
-  -mapper mapper.py \
-  -reducer cat \
-  -file mapper.py \
-  -file language_station_map.json
+  END_TIME=$(date +%s)
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] END $1 - Duration: $((END_TIME - START_TIME))s" >> $LOGFILE
+  echo "" >> $LOGFILE
+}
 
-hadoop jar $HADOOP_STREAMING_JAR \
-  -input $TMP3_OUT \
-  -output $FINAL_OUT \
-  -mapper cat \
-  -reducer reducer.py \
-  -file reducer.py
+log "Map MIMIC" \
+  "hadoop jar $HADOOP_STREAMING_JAR \
+    -input $INPUT_MIMIC \
+    -output $TMP1_OUT \
+    -mapper mapper_mimic.py \
+    -reducer cat \
+    -file mapper_mimic.py"
+
+log "MapReduce GDELT" \
+  "hadoop jar $HADOOP_STREAMING_JAR \
+    -input $INPUT_GDELT \
+    -output $TMP2_OUT \
+    -mapper mapper_gdelt.py \
+    -reducer reducer_gdelt.py \
+    -file mapper_gdelt.py \
+    -file reducer_gdelt.py"
+
+log "Join MIMIC + GDELT" \
+  "hadoop jar $HADOOP_STREAMING_JAR \
+    -input $TMP1_OUT \
+    -input $TMP2_OUT \
+    -output $TMP3_OUT \
+    -mapper mapper.py \
+    -reducer cat \
+    -file mapper.py \
+    -file language_station_map.json"
+
+log "Final Reduction & Aggregation" \
+  "hadoop jar $HADOOP_STREAMING_JAR \
+    -input $TMP3_OUT \
+    -output $FINAL_OUT \
+    -mapper cat \
+    -reducer reducer.py \
+    -file reducer.py"
 
 hdfs dfs -cat $FINAL_OUT/part-* | head -n 10
